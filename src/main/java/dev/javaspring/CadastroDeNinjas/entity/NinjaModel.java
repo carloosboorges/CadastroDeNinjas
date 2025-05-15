362e078434b4ff7caa512a54d6fe2848a0141b3c
carloosboorges <carloos-boorges@hotmail.com>
2025-05-15 17:21:16 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel

---
+++ src/main/java/dev/javaspring/CadastroDeNinjas/entity/NinjaModel.java
@@ -0,0 +1,56 @@
+package dev.javaspring.CadastroDeNinjas.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity // Transforma uma classe em uma entidade do banco de dados.
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Nome", )
+    private String nome;
+
+    @Column(name = "Idade")
+    private int idade;
+
+    @Column(name = "Email")
+    private String email;
+
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
