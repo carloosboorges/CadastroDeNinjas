51a0ddf3702fe8bad17c4d698af86068c85f58e7
carloosboorges <carloos-boorges@hotmail.com>
2025-06-02 20:32:39 -0300

    feat: Adicionado metodo findbyEmail no ninjaRepository

--- src/main/java/dev/javaspring/CadastroDeNinjas/repository/NinjaRepository.java
+++ src/main/java/dev/javaspring/CadastroDeNinjas/repository/NinjaRepository.java
@@ -3,11 +3,13 @@ import dev.javaspring.CadastroDeNinjas.entity.NinjaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 
 
-
-
+    boolean id(Long id);
+    Optional<NinjaModel> findByEmail(String email);
 }
